f7ed61ff6ad870e2a85ddc091c3d1c5e5a62d81b
xushuai <dyxushuai@gmail.com>
2017-07-14 14:55:18 +0800

    Rename error message

--- selector.go
+++ selector.go
@@ -14,13 +14,13 @@ import (
 
 var (
 	// ErrNoServer when no server has found then return error.
-	ErrNoServer = errors.New("consistent hasher: There is no server")
+	ErrNoServer = errors.New("server selector: There is no server")
 	// ErrServerExisted the server has existed when add to the ServerSelector.
-	ErrServerExisted = errors.New("consistent hasher: Server has existed")
+	ErrServerExisted = errors.New("server selector: Server has existed")
 	// ErrServerNotExisted the server has not existed when delete from ServerSelector.
-	ErrServerNotExisted = errors.New("consistent hasher: Server has not existed")
+	ErrServerNotExisted = errors.New("server selector: Server has not existed")
 	// ErrUnsupportOp operation must be in Add or Delete
-	ErrUnsupportOp = errors.New("consistent hasher: Unsupport operation, must be one of Add or Delete")
+	ErrUnsupportOp = errors.New("server selector: Unsupport operation, must be one of Add or Delete")
 )
 
 // ServerSelector decide which server can be used.
